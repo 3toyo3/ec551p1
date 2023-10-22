ef16f123eb97efcc83dbf71eec133139749ac37a
yt <o@dm.com>
2023-10-22 19:07:34 -0400

    addressed todos, verified sop, added translate function, added demo files

--- ec551p1.py
+++ ec551p1.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 from collections import defaultdict
 
 # assumes that the file format is a certain way, as well as in the same directory
@@ -11,13 +12,10 @@ model = ""
 inputs = []
 outputs = []
 names = defaultdict(list)
-
-if __name__ == "__main__":
-	main()
-
+names_tt = {}
 
 def main():
-	 if len(sys.argv) != 3:
+	if len(sys.argv) != 3:
 		print("Usage: python ec551p1.py <file> <1-12>")
 		sys.exit()
 	else:
@@ -33,148 +31,212 @@ def main():
 	#TODO clean outputs here, check input outputs
 	#1. return design as a canonical sop
 	if command == "1":
-		sop_c()
+		sops = sop_c()
+		print(sops)
 	#2. return design as a canonical pos
-	else if command == "2":
-		print("two")
+	elif command == "2":
+		poss = pos_c()
+		print(poss)
 	#3. return design as a inverse canonical sop
-	else if command == "3":
-		print("three")
+	elif command == "3":   #TODO check
+		sops = sop_c()
+		print(sops)
+		inverted = inverse(sops)
+		print(inverted)
 	#4. return design as a inverse canonical pos
-	else if command == "4":
-		print("four")
+	elif command == "4":
+		poss = pos_c()
+		inverted = inverse(poss)
+		print(inverted)
 	#5. return a minimized number of literals representation in sop
-	else if command == "5":
+	elif command == "5":
 		print("five")
 		print("Saved literals:")
 	#6. return a minimized number of literals representation in pos
-	else if command == "6":
+	elif command == "6":
 		print("six")
 		print("Saved literals:")
 	#7. return the number of prime implicants
-	else if command == "7":
-		int primp = 0
+	elif command == "7":
 		print("seven")
 	#8. return the number of essential prime implicants
-	else if command == "8":
+	elif command == "8":
 		print("eight")
 	#9. return number of on-set minterms
-	else if command == "9":
+	elif command == "9":
 		print("nine")
 	#10. return number of on-set maxterms
-	else if command == "10":
+	elif command == "10":
 		print("ten")
 	#11. ???
-	else if command == "11":
+	elif command == "11":
 		print("eleven")
 	#12. ???
-	else if command == "12":
+	elif command == "12":
 		print("twelve")
 	else:
 		print("Unknown command: ", command)
 		sys.exit()
 
 def parser(filename):
+	global model
+	global inputs
+	global outputs
+	global names
 	currentname = None
 	with open(filename, "r") as file:
 		for line in file:
 			if line.startswith(".model"):
 				model += line[len(".model"):].strip()
-				print(model)
+				print("Model:" + model)
 			elif line.startswith(".inputs"):
 				ins = line[len(".inputs"):].strip().split()
 				inputs.extend(ins)
+				print("Inputs:")
 				print(inputs)
 			elif line.startswith(".outputs"):
 				outs = line[len(".outputs"):].strip().split()
 				outputs.extend(outs)
+				print("Outputs:")
 				print(outputs)
 			#store each "module" of blif
 			elif line.startswith(".names"):
 				currentname = line[len(".names"):].strip()
-				print(currentname)
+				#print(currentname)
 			elif line.startswith(".end"):
 				break
 			elif currentname is not None:
-				names[currentname].append(line)
-				print(names[currentname])
+				#print("Current line: "+ line )
+				names[currentname].append(line.rstrip())
+				#print(names[currentname])
 			else:
 				pass
-	tt() # finish cleaning by turning rows into tt
+	truet() # finish cleaning by turning rows into tt
 
-def tt(): #make truth table for each name, clean up lines and expand to master copy :)
+def truet(): #make truth table for each name, clean up lines and expand to master copy :)
+	global names
 	for entry in names:
-		tt = []
-		print(entry + "," names[entry])
+		truet = []
+		#print(entry)
 
 		#get true values of the chart
 		for lines in names[entry]:
 			cleanline = lines.rstrip()
-			print(cleanline)
-			if cleanline.endswith('1')
+			#print("Cleaned line: "+ cleanline)
+			if cleanline.endswith('1'):
 				cleanline = cleanline[:-1].rstrip()
-				print(cleanline, " with True removed")
+				#print(cleanline, " with True removed")
 				tt.append(cleanline)
-		print(tt)
-
-		#TODO check that number of variables match length?
+		#print("Truth table for :" + entry)
+		#print(tt)
 
 		expanded=[]
-		ref_tt = tt[:] #TODO make sure this copies
+		ref_truet = truet[:]
 
 		#look for minimized functions
-		for row in ref_tt:
-			print(row)
-			if '-' in row: #TODO make sure right characters
-				expand_row = row
-				tt.remove(row) #check that this removes properly IE not messing up index
+		for row in ref_truet:
+			#print("Working on: " + row)
+			if '-' in row:
+				expand_row = list(row)
+				truet.remove(row) #check that this removes properly IE not messing up index
 				#replace multiple dont cares
 				for i, char in enumerate(expand_row):
 					if char == '-':
 						expand_row[i]='0'
-						print(expand_row)
-						expanded.append(expand_row)
+						#print(expand_row)
+						expanded.append(''.join(expand_row))
 						expand_row[i]='1'
-						expanded.append(expand_row)
-						print(expand_row)
-		ref_tt.clear()
+						expanded.append(''.join(expand_row))
+						#print(expand_row)
+		ref_truet.clear()
 
 		# add back expanded and put in ref for later
-		tt.append(expanded)
-		tt = list(set(tt)) #TODO check that this removes dupes
-		print(tt)
+		truet.extend(expanded)
+		print("Truth table for " + entry)
+		#print(tt)
+		truet = list(set(truet)) #removes dupes
+		print(truet)
 		#TODO maybe convert to binary
-		names[entry] = tt
+		names[entry] = truet
+
+def tt_translate():
+	global names
+	global names_tt
+	for entry in names: # variable count check
+		truthtable=np.zeros((2,2,2,2))
+		if len(names[entry][0]) < 4:
+			if len(names[entry][0] == 2):
+				for row in names[entry]:
+					A = row[0]
+					B = row[1]
+					truthtable[A][B][0][0] = 1
+					truthtable[A][B][0][1] = 1
+					truthtable[A][B][1][1] = 1
+					truthtable[A][B][1][0] = 1
+			elif len(names[entry][0] == 3):
+				for row in names[entry]:
+					A = row[0]
+					B = row[1]
+					C = row[2]
+					truthtable[A][B][C][0] = 1
+					truthtable[A][B][C][1] = 1
+			elif len(names[entry][0] == 1):
+				for row in names[entry]:
+					A = row[0]
+					truthtable[A][0][0][0] = 1
+					truthtable[A][0][0][1] = 1
+					truthtable[A][0][1][1] = 1
+					truthtable[A][0][1][0] = 1
+					truthtable[A][1][0][0] = 1
+					truthtable[A][1][0][1] = 1
+					truthtable[A][1][1][1] = 1
+					truthtable[A][1][1][0] = 1
+			else:
+				print("Something is wrong line 193")
+	
+		elif len(names[entry][0] == 4): #if equal to 4
+			for row in names[entry]:
+				A = row[0]
+				B = row[1]
+				C = row[2]
+				D = row[3]
+				truthtable[A][B][C][D] = 1
+		else:
+			printf("Representing this into a 4x4 array is beyond this program.")
+		names_tt[entry] = truthtable
 
 def sop_c():
+	global names
 	canonicals = []
 	for entry in names:
 		canoneqn = ""
-		variables = entry.split().upper()
-		print(entry + "," names[entry])
+		variables = entry.upper().split()
+		#print(entry)
+		#print(names[entry])
 
 		canoneqn += f"{variables[-1]}="
 		variables.pop() #remove output
 
 		for row in names[entry]:
-			print(row)
-			row_dupe = row
+			#print(row)
+			row_dupe = list(row)
 			#TODO think more
-			for i in range(len(row)): #TODO get rid of *
+			for i in range(len(row)): 
 				if row[i] == '1':
-					row_dupe[i] = variable[i]
+					row_dupe[i] = variables[i]
 				elif row[i] == '0': #Lower to rep 0 value
-					row_dupe[i] = variable[i].lower()
-				else
+					row_dupe[i] = variables[i].lower()
+				else:
 					print("Something is wrong!!")
-			print(row_dupe)
-			canoneqn += row_dupe+"+"
+			#print(row_dupe)
+			canoneqn +=''.join(row_dupe)+"+"
 		canoneqn = canoneqn[:-1] #remove last plus
-		canonicals.append(canoneqn) #TODO or return canonical
+		canonicals.append(canoneqn)
 	return canonicals
 
-def pos_c(): #TODO make into variable names
+def pos_c(): #TODO make into variable names FIX
 	canonicals = []
+	global names
 	for entry in names:
 		canonicaleqn = "G=("
 		for row in names[entry]:
@@ -185,11 +247,11 @@ def pos_c(): #TODO make into variable names
 					row_dupe[i] = 0
 				elif row_dupe[i] == 0:
 					row_dupe[i] = 1
-				else
+				else:
 					pass
 			canonicaleqn += row_dupe +")("
 		canonicaleqn = canonicaleqn[:-1]
-		canonical.append(canonicaleqn)
+		canonicals.append(canonicaleqn)
 	return canonicals
 
 #def demorgan(): #idk if i need to do this afterall
@@ -201,6 +263,13 @@ def inverse(output): #input the sop or pos things
 		inverts.append(inverted)
 	return inverts
 
-def minimizer(): #shout out quinemckluskey
+#def minimizer(): #shout out quinemckluskey
 
 #TODO onset minterms, onset maxterms, 11 and 12
+
+# 11 kmap
+# 12 hazards
+
+if __name__ == "__main__":
+	main()
+ 
